877bd764d5a4745cedda6759c92cf460d174114f
Zach Yang <18612568615@163.com>
2024-03-18 20:56:38 -0700

    Add files via upload

---
+++ E9.1/e91.py
@@ -0,0 +1,139 @@
+import torch
+from torch import nn
+from torch._C import device
+from torchvision import transforms
+from torchvision import datasets
+from torch.utils.data import DataLoader
+import torch.nn.functional as F
+import torch.optim as optim
+import matplotlib.pyplot as plt
+
+batch_size = 64
+transform = transforms.Compose(
+    [transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))]
+)
+train_dataset = datasets.MNIST(
+    root="./dataset/mnist/", train=True, download=False, transform=transform
+)
+train_loader = DataLoader(train_dataset, shuffle=True, batch_size=batch_size)
+test_dataset = datasets.MNIST(
+    root="./dataset/mnist/", train=False, download=False, transform=transform
+)
+test_loader = DataLoader(test_dataset, shuffle=False, batch_size=batch_size)
+
+
+class InceptionA(nn.Module):
+    def __init__(self, in_channels):
+        super(InceptionA, self).__init__()
+        self.branch1x1 = nn.Conv2d(in_channels, 16, kernel_size=1)
+
+        self.branch5x5_1 = nn.Conv2d(in_channels, 16, kernel_size=1)
+        self.branch5x5_2 = nn.Conv2d(16, 24, kernel_size=5, padding=2)
+
+        self.branch3x3_1 = nn.Conv2d(in_channels, 16, kernel_size=1)
+        self.branch3x3_2 = nn.Conv2d(16, 24, kernel_size=3, padding=1)
+        self.branch3x3_3 = nn.Conv2d(24, 24, kernel_size=3, padding=1)
+
+        self.branch_pool = nn.Conv2d(in_channels, 24, kernel_size=1)
+
+    def forward(self, x):
+        branch1x1 = self.branch1x1(x)
+
+        branch5x5 = self.branch5x5_1(x)
+        branch5x5 = self.branch5x5_2(branch5x5)
+
+        branch3x3 = self.branch3x3_1(x)
+        branch3x3 = self.branch3x3_2(branch3x3)
+        branch3x3 = self.branch3x3_3(branch3x3)
+
+        branch_pool = F.avg_pool2d(x, kernel_size=3, stride=1, padding=1)
+        branch_pool = self.branch_pool(branch_pool)
+
+        outputs = [branch1x1, branch5x5, branch3x3, branch_pool]
+        return torch.cat(outputs, dim=1)
+
+
+class GNet(nn.Module):
+    def __init__(self):
+        super(GNet, self).__init__()
+        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
+        self.conv2 = nn.Conv2d(88, 20, kernel_size=5)
+
+        self.incept1 = InceptionA(in_channels=10)
+        self.incept2 = InceptionA(in_channels=20)
+
+        self.mp = nn.MaxPool2d(2)
+        self.fc1 = nn.Linear(1408, 512)
+        self.fc2 = nn.Linear(512, 10)
+        # self.fc3 = nn.Linear(128, 10)
+
+    def forward(self, x):
+        in_size = x.size(0)
+        x = F.relu(self.mp(self.conv1(x)))
+        x = self.incept1(x)
+        x = F.relu(self.mp(self.conv2(x)))
+        x = self.incept2(x)
+        x = x.view(in_size, -1)
+        # print(x.size(1))  # 查看计算后的维度
+        x = F.relu(self.fc1(x))
+        # x = F.relu(self.fc2(x))
+        # x = self.fc3(x)
+        x = self.fc2(x)
+        return x
+
+
+model = GNet()
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+model.to(device)
+criterion = torch.nn.CrossEntropyLoss()
+optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.5)
+
+
+def train(epoch):
+    running_loss = 0.0
+    for batch_idx, data in enumerate(train_loader, 0):
+        inputs, target = data
+        inputs, target = inputs.to(device), target.to(device)
+        optimizer.zero_grad()
+
+        outputs = model(inputs)
+        loss = criterion(outputs, target)
+        loss.backward()
+        optimizer.step()
+
+        running_loss += loss.item()
+        if batch_idx % 300 == 299:
+            print(
+                "[%d, %3d] loss: %.3f" % (epoch + 1, batch_idx + 1, running_loss / 300)
+            )
+            running_loss = 0.0
+
+
+def test(acc_list):
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for data in test_loader:
+            images, labels = data
+            images, labels = images.to(device), labels.to(device)
+            outputs = model(images)
+            _, predicted = torch.max(outputs.data, dim=1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+    print("Accuracy on test set: %2.2f %%" % (100 * correct / total))
+    acc_list.append(100 * correct / total)
+
+
+if __name__ == "__main__":
+    epoch_list = []
+    acc_list = []
+    for epoch in range(10):
+        train(epoch)
+        epoch_list.append(epoch)
+        test(acc_list)
+
+    plt.plot(epoch_list, acc_list)
+    plt.xlabel("epoch")
+    plt.ylabel("accuracy")
+    plt.grid()
+    plt.show()
